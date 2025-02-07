9ebbec5aa9b93734f91ee34ffd7652e365219764
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-02-07 08:39:11 +0000

    Create base structure of code

---
+++ src/main/java/ru/gavrilovegor519/hh_autoupdate_resume/HhAutoupdateResumeApplication.java
@@ -0,0 +1,13 @@
+package ru.gavrilovegor519.hh_autoupdate_resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhAutoupdateResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhAutoupdateResumeApplication.class, args);
+	}
+
+}
