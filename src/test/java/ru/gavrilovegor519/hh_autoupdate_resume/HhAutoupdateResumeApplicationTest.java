9f4cf1bb8463bcca605086b811d6fe8750ababa3
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-02-08 01:57:14 +0300

    Add context load test.

---
+++ src/test/java/ru/gavrilovegor519/hh_autoupdate_resume/HhAutoupdateResumeApplicationTest.java
@@ -0,0 +1,14 @@
+package ru.gavrilovegor519.hh_autoupdate_resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HhAutoupdateResumeApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
